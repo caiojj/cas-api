7878e41a1ff485e1762170d90ef70ab527a18a57
caiojj <leal22caio@gmail.com>
2021-09-12 18:29:52 -0300

    Implementação da função de login

--- src/api/user.js
+++ src/api/user.js
@@ -29,16 +29,36 @@ module.exports = app => {
         ]
 
         try {
-            await app.db.batch(query, { prepare: true })
+            await app.db.batch(query, { prepare: true }, (err, result) => {
+                if (err) res.status(500).send(err)
+                else res.status(200).send()
+            })
         }catch(msg) {
             res.status(500).send(`Erro no servidor.\n ${msg}`)
         }
-        res.status(200).send("Cadastro finalizado com sucesso!")
     }
 
     const login = async (req, res) => {
-        return res.status(200).send()
+        const user = { ...req.body }
+        
+        try {
+            isNullValue(user.email, "Preencha o campo de email.")
+            isNullValue(user.password, "Preencha o campo senha.")
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+        
+        const query = "SELECT * FROM user WHERE email = ? AND password = ? ALLOW FILTERING"
+        const params = [user.email, user.password]
+        app.db.execute(query, params)
+          .then(users => {
+            if (users["rows"].length === 0) {
+                return res.status(400).send("Usuario não esta cadastrado.")
+            }
+            return res.status(200).send(res.json(users["rows"]))
+        })
+          .catch(err => res.status(500).send(err))
     }
-    
+
     return { createAccount, login }
 }
